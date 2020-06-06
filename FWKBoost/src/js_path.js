b5667cf23fc2d64fd37f384330d9200851e42b3b
Labsoweb\babsolune <babsolune@phpboost.com>
2020-06-06 13:40:33 +0200

    [UPD] variables

--- FWKBoost/src/js_path.js
+++ FWKBoost/src/js_path.js
@@ -6,6 +6,10 @@ function loadScript(url)
     jsLocation.appendChild(script);
 }
 
+var L_HIDE_MESSAGE    = 'tag_hide_message';
+var L_HIDE_HIDEBLOCK  = 'tag_hide_hideblock';
+var L_COPYTOCLIPBOARD = 'Copy to clipboard';
+
 loadScript('https://raw.githack.com/PHPBoost/PHPBoost/master/templates/default/plugins/@global.js');
 loadScript('https://raw.githack.com/PHPBoost/PHPBoost/master/templates/default/plugins/autobox.js');
 loadScript('https://raw.githack.com/PHPBoost/PHPBoost/master/templates/default/plugins/autocomplete.js');
