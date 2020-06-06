7be6f659e695f2bd8577cb1543fe35db519bda53
Labsoweb\babsolune <babsolune@phpboost.com>
2020-06-06 09:26:44 +0200

    [UPD] lightbox + favicon

--- FWKBoost/src/phpboost.init.js
+++ FWKBoost/src/phpboost.init.js
@@ -1,75 +1,95 @@
 // js_bottom.tpl
 $(document).ready(function(){
-    // All tables
-    	jQuery('.table').basictable();
-    	jQuery('.table-no-header').basictable({
-    	    header: false
-    	});
+// lightbox
+	jQuery(document).ready(function() {
+		update_data_confirmations();
+		jQuery('a[rel^=lightbox]').attr('data-rel', 'lightcase:collection');
+		jQuery('a[data-lightbox^=formatter]').attr('data-rel', 'lightcase:collection');
+		jQuery('a[data-rel^=lightcase]').lightcase({
+			labels : {
+				'errorMessage'    : 'element.unexist',
+				'sequenceInfo.of' : ' of ',
+				'close'           : 'close',
+				'navigator.prev'  : 'previous',
+				'navigator.next'  : 'next',
+				'navigator.play'  : 'play',
+				'navigator.pause' : 'pause'
+			},
+			maxHeight: window.innerHeight,
+			maxWidth: window.innerWidth,
+			shrinkFactor: 0.85
+		});
+	});
+// All tables
+	jQuery('.table').basictable();
+	jQuery('.table-no-header').basictable({
+	    header: false
+	});
 
-    // line numbers in <code>
-    	jQuery(function() {
-    		jQuery(".lined textarea").linedtextarea();
-    	});
+// line numbers in <code>
+	jQuery(function() {
+		jQuery(".lined textarea").linedtextarea();
+	});
 
-    // Delete captcha fielset if captcha is active when user is connected
-    	if(jQuery('.captcha-element .form-element').length == 0)
-    		jQuery('.captcha-element').removeClass('wizard-step');
+// Delete captcha fielset if captcha is active when user is connected
+	if(jQuery('.captcha-element .form-element').length == 0)
+		jQuery('.captcha-element').removeClass('wizard-step');
 
-    // Multitabs
-        jQuery('.modal-container [data-modal]').multiTabs({ pluginType: 'modal' });
-        jQuery('.accordion-container.basic [data-accordion]').multiTabs({ pluginType: 'accordion'});
-        jQuery('.accordion-container.siblings [data-accordion]').multiTabs({ pluginType: 'accordion', accordionSiblings: true });
-        jQuery('.tabs-container [data-tabs]').multiTabs({ pluginType: 'tabs' });
+// Multitabs
+    jQuery('.modal-container [data-modal]').multiTabs({ pluginType: 'modal' });
+    jQuery('.accordion-container.basic [data-accordion]').multiTabs({ pluginType: 'accordion'});
+    jQuery('.accordion-container.siblings [data-accordion]').multiTabs({ pluginType: 'accordion', accordionSiblings: true });
+    jQuery('.tabs-container [data-tabs]').multiTabs({ pluginType: 'tabs' });
 
-    // Wizard
-        jQuery('.wizard-container').wizard();
+// Wizard
+    jQuery('.wizard-container').wizard();
 
-    // Selectimg
-    	jQuery('.select-to-list').selectimg({
-    		ariaLabel : 'click.to.select'
-    	});
+// Selectimg
+	jQuery('.select-to-list').selectimg({
+		ariaLabel : 'click.to.select'
+	});
 
-    // sizes of .cell-thumbnail
-    	jQuery('.cell-thumbnail.cell-landscape').each(function() {
-    		var widthRef = $(this).innerWidth();
-    		$(this).outerHeight(widthRef * 9 / 16);
-    	});
-    	jQuery('.cell-thumbnail.cell-square').each(function() {
-    		var widthRef = $(this).innerWidth();
-    		$(this).outerHeight(widthRef);
-    	});
-    	jQuery('.cell-thumbnail.cell-portrait').each(function() {
-    		var widthRef = $(this).innerWidth();
-    		$(this).outerHeight(widthRef * 16 / 9);
-    	});
+// sizes of .cell-thumbnail
+	jQuery('.cell-thumbnail.cell-landscape').each(function() {
+		var widthRef = $(this).innerWidth();
+		$(this).outerHeight(widthRef * 9 / 16);
+	});
+	jQuery('.cell-thumbnail.cell-square').each(function() {
+		var widthRef = $(this).innerWidth();
+		$(this).outerHeight(widthRef);
+	});
+	jQuery('.cell-thumbnail.cell-portrait').each(function() {
+		var widthRef = $(this).innerWidth();
+		$(this).outerHeight(widthRef * 16 / 9);
+	});
 
-    	jQuery(window).ready(function() {
-      		jQuery('.body-wrapper').animate({opacity: 1}, 300);
-    	});
+	jQuery(window).ready(function() {
+  		jQuery('.body-wrapper').animate({opacity: 1}, 300);
+	});
 
-    // Autoresize Textareas
-    	$(document).autoboxOn('textarea:not(.lined textarea)');
+// Autoresize Textareas
+	$(document).autoboxOn('textarea:not(.lined textarea)');
 
-    // Add a colored square to the element and color its borders if it has
-     	jQuery('[data-color-surround]').colorSurround();
+// Add a colored square to the element and color its borders if it has
+ 	jQuery('[data-color-surround]').colorSurround();
 
-    // Owl Carousel
-     	jQuery('[id*="slideboost"] > br').remove();
-    	jQuery('[id*="slideboost"]')
-    		.addClass('owl-carousel')
-    		.owlCarousel({
-    			autoplay: true,
-    			autoplayTimeout: 2000,
-    			loop: true,
-    			margin: 15,
-    			smartSpeed: 1000,
-    			autoplayHoverPause: true,
-    			responsive: {
-    				0: { items: 1},
-    				768: { items: 2},
-    				1024: { items: 3},
-    				1366: { items: 4}
-    			}
-    	});
+// Owl Carousel
+ 	jQuery('[id*="slideboost"] > br').remove();
+	jQuery('[id*="slideboost"]')
+		.addClass('owl-carousel')
+		.owlCarousel({
+			autoplay: true,
+			autoplayTimeout: 2000,
+			loop: true,
+			margin: 15,
+			smartSpeed: 1000,
+			autoplayHoverPause: true,
+			responsive: {
+				0: { items: 1},
+				768: { items: 2},
+				1024: { items: 3},
+				1366: { items: 4}
+			}
+	});
 
 });
